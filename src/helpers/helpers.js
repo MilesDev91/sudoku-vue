c5e78fac35373cf7d644769eae380b0c30f00f7e
Miles Coffin <miles.coffin@joinkllc.com>
2023-06-23 13:55:39 -0400

    Added column validation. Renamed variables on both validation functions for better clarity.

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -28,21 +28,21 @@ const validateRow = (grid, gridErrors) => {
     // Check for duplicates in each row by adding a key:value to 
     // alreadyseen, then checking on each subsequent loop.
     // If a match is found, then add the 2d location to gridErrors
-    for(let i in grid) {
-    let alreadySeen = {}
-        for(let y in grid[i]) {
-            if(alreadySeen[grid[i][y]]) {
+    for(let row in grid) {
+        let alreadySeen = {}
+        for(let column in grid) {
+            if(alreadySeen[grid[row][column]]) {
                 // There is probably a better way to do this,
                 // but we need to loop back through the row to get
                 // the previous location.
-                for(let x in grid[i]) {
-                    if(grid[i][y] == grid[i][x]) {
-                        gridErrors[i][x] = true;
+                for(let x in grid) {
+                    if(grid[row][x] && grid[row][column] == grid[row][x]) {
+                        gridErrors[row][x] = true;
                     }
                 }
             }
             else {
-                alreadySeen[grid[i][y]] = true;
+                alreadySeen[grid[row][column]] = true;
             }
         }
     }
@@ -50,7 +50,25 @@ const validateRow = (grid, gridErrors) => {
 
 // Validate column
 const validateColumn = (grid, gridErrors) => {
-    return
+    // Use a similar method as above, just with different looping.
+    // Used different syntax as well, but does the same thing.
+    for(let column = 0; column < 9; column++) {
+        let alreadySeen = {}
+        for(let row = 0; row < 9; row++) {
+            if(alreadySeen[grid[row][column]]) {
+                // loop back through to find all locations of duplicates
+                for(let x = 0; x < 9; x++) {
+                    if(grid[x][column] && grid[row][column] == grid[x][column]) {
+                        gridErrors[x][column] = true;
+                    }
+                }
+            }
+
+            else {
+                alreadySeen[grid[row][column]] = true;
+            }
+        }
+    }
 }
 
 // Validate block
