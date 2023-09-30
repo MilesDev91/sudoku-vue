13250e18c51975ef5bc10c6c4bb0cfe80b070341
Miles Coffin <34494229+MilesDev91@users.noreply.github.com>
2023-09-30 17:00:56 -0400

    Pencil Mark Foundation
    Laid foundation for pencil marking.

--- src/components/Solver/Solver.vue
+++ src/components/Solver/Solver.vue
@@ -8,20 +8,24 @@
             :gridErrors="gridErrors"
         />
         <button @click="solveGrid()">Solve</button>
+        <button @click="autopencil()">Auto Pencil</button>
     </div>
 </template>
 
 <script setup>
 import validateGrid from './../../helpers/validation';
 import findSolution from './../../helpers/solve';
+import pencilGrid from './../../helpers/pencil';
 import { ref, watch } from 'vue';
 
 // 2d array, Array[row][column]
 const grid = ref([...Array(9)].map(e => Array(9)));
 const gridErrors = ref([...Array(9)].map(e => Array(9)));
 const selectedCell = ref();
-// const pencilMarkGrid = ref([...Array(9)].map(e => Array(9)));
-const pencilMarkGridSolve = ref([...Array(9)]
+const pencilMarkGrid = ref([...Array(9)]
+    .map(e => [...Array(9)]
+    .map(e => [...Array(9)])));
+const solvingGrid = ref([...Array(9)]
     .map(e => [...Array(9)]
     .map(e => [1,2,3,4,5,6,7,8,9])));
 
@@ -29,8 +33,12 @@ const changeGridCellValue = (value, row, column) => {
     grid._value[row][column] = value;
 }
 
+const autopencil = () => {
+    pencilMarkGrid.value = pencilGrid(grid.value);
+}
+
 const solveGrid = () => {
-    findSolution(grid.value, pencilMarkGridSolve.value);
+    findSolution(grid.value, solvingGrid.value);
 }
 
 watch(() => grid.value, (grid) => {
---
+++ src/helpers/pencil.js
@@ -0,0 +1,19 @@
+// Because pencil marks don't care about duplicates, we are going to do penciling slightly different from validation.
+// Instead of cycling through each row, column, and block, we are cycling through each cell until we hit a value, then
+// we cycle through every row, column, and block that matches the cell.
+
+export default function pencilGrid (grid, pencilGrid) {
+    return;
+}
+
+function pencilRow (pencilGrid, cell, value) {
+    return;
+}
+
+function pencilColumn (pencilGrid, cell, value) {
+    return;
+}
+
+function pencilBlock (pencilGrid, cell, value) {
+    return;
+}
