5f868a892260569b79dfa9743573045674ca2214
Miles Coffin <34494229+MilesDev91@users.noreply.github.com>
2024-02-12 20:42:22 -0500

    Add files via upload

---
+++ src/interfaces/Grid.ts
@@ -0,0 +1,6 @@
+/**
+ * 
+ */
+export default interface Grid {
+    cells: number[][]
+}
---
+++ src/interfaces/SelectedCell.ts
@@ -0,0 +1,3 @@
+export default interface SelectedCell {
+    coordinate: [value1: number, value2: number]
+}
